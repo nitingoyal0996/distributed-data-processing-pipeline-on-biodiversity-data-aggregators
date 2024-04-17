f3021b0bcd0af78838204c75e4250bcc9b99df16
root <root@node0.ngoyal-200000.ufl-eel6761-sp24-pg0.utah.cloudlab.us>
2024-04-16 20:50:55 -0600

    update topic scripts to handle retention time

--- scripts/kafka/create-topic.sh
+++ scripts/kafka/create-topic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kafka-topics.sh --bootstrap-server 1@128.110.217.192:9092,2@128.110.217.163:9092,3@128.110.217.175:9092 --create --topic 'gbif' --partitions 1
+kafka-topics.sh --bootstrap-server 128.110.217.192:9092,128.110.217.163:9092,128.110.217.175:9092 --create --topic 'gbif' --partitions 1 --config retention.ms=10000
