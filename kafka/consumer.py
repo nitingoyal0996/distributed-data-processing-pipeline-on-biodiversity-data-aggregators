32231116bede5f1b37f8a3611d12016e614abde4
root <root@node0.ngoyal-200000.ufl-eel6761-sp24-pg0.utah.cloudlab.us>
2024-04-16 20:56:07 -0600

    update consumer script

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,21 +1,25 @@
 import sys
 sys.path.append('/users/ngoyal')
 
-import os
 from confluent_kafka import Consumer
 # from src.utils.interfaces.singleton import Singleton
 
 class MyConsumer():
     
-    
-    def __init__(self):
+    def __init__(self, servers):
         
         conf = {
-            'bootstrap.servers': '1@128.110.217.192:9092,2@128.110.217.163:9093,3@128.110.217.175:9094',
+            'bootstrap.servers': servers,
             'group.id': 'local_consumer'
         }
         self.my_consumer = Consumer(conf)
-                
+        print(conf)
+        
+        
+    def print_assignment(self, consumer, partitions):
+        print('Assignment:', consumer)
+
+      
     def consume_topic(self, topic_name):
         print(topic_name)
-        self.my_consumer.subscribe([topic_name])
+        self.my_consumer.subscribe([topic_name], on_assign=self.print_assignment)
